521fc0021ab8de2ca956263588a780c8ba31ec25
juliannagevchuk <gevchuk@gmail.com>
2018-05-13 17:37:10 -0400

    Delete vendor files, cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,6 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
-// Copy third party libraries from /node_modules into /vendor
-gulp.task("vendor", function() {
-  // Bootstrap
-  gulp
-    .src([
-      "./node_modules/bootstrap/dist/**/*",
-      "!./node_modules/bootstrap/dist/css/bootstrap-grid*",
-      "!./node_modules/bootstrap/dist/css/bootstrap-reboot*"
-    ])
-    .pipe(gulp.dest("./vendor/bootstrap"));
-});
-
 gulp.task("css", function() {
   return gulp
     .src("src/sass/style.scss")
@@ -31,5 +19,3 @@ gulp.task("watch", function() {
   gulp.watch("src/sass/*.scss", ["css"]);
 });
 
-// Default task
-gulp.task("default", ["vendor"]);
